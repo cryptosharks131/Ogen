5fc758318ba87a7f5be7acb092f36ffef9984c72
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2020-06-21 15:11:30 -0500

    updates

--- ogen_install_testnet.sh
+++ ogen_install_testnet.sh
@@ -107,6 +107,7 @@ function create_validators() {
   REQ_BALANCE=`expr $NUM_VALIDATORS \* 100`
   echo -e "A ${RED}balance of $REQ_BALANCE${NC} is required to start your validators. Press any key to continue after deposit is made."
   read -e
+  curl -X GET localhost:8080/wallet/getbalance
 }
 
 #Start Script
