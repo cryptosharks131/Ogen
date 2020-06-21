1242e6b90510d5aaf82e1ece65215fcc3d0b5f75
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2020-06-21 08:46:14 -0500

    Create install file

---
+++ ogen_install.sh
@@ -0,0 +1,47 @@
+TMP_FOLDER=$(mktemp -d)
+COIN_REPO='https://public.oly.tech/olympus/release/v0.0.1/ogen-0.0.1-linux-amd64.tar.gz'
+COIN_NAME='Olympus'
+RED='\033[0;31m'
+NC='\033[0m'
+
+function compile_error() {
+  if [ "$?" -gt "0" ];
+    then
+    echo -e "${RED}Failed to compile $COIN_NAME. Please investigate.${NC}"
+    printf "${NC}"
+    exit 1
+  fi
+}
+
+function initialize() {
+  if [ ! -d "$HOME/.config/" ]; then
+    mkdir $HOME/.config/
+  fi
+}
+
+function install_node() {
+  echo -e "Prepare to download $COIN_NAME"
+  cd $TMP_FOLDER
+  wget -q $COIN_REPO
+  compile_error
+  COIN_ZIP=$(echo $COIN_REPO | awk -F'/' '{print $NF}')
+  tar xvf $COIN_ZIP --strip 2 >/dev/null 2>&1
+  compile_error
+  cp ogen /usr/local/bin
+  compile_error
+  cd - >/dev/null 2>&1
+  rm -rf $TMP_FOLDER >/dev/null 2>&1
+  clear
+}
+
+function start_node() {
+  ogen reset
+  ogen --enablemining=true
+  sleep 10
+}
+
+#Start Script
+clear
+
+initialize
+install_node
