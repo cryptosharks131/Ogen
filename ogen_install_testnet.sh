dc7eb32aee931229528f4b12c9a578a555e2d545
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2020-08-03 15:11:59 -0500

    update link

--- ogen_install_testnet.sh
+++ ogen_install_testnet.sh
@@ -1,5 +1,5 @@
 TMP_FOLDER=$(mktemp -d)
-COIN_REPO='https://public.oly.tech/olympus/release/v0.0.1/ogen-0.0.1-linux-amd64.tar.gz'
+COIN_REPO='https://public.oly.tech/olympus/ogen-release/ogen-0.0.1-linux-amd64.tar.gz'
 COIN_NAME='Olympus'
 COIN_DAEMON='ogen'
 RED='\033[0;31m'
