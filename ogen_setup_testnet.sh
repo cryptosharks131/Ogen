d55ac5ac3b39502d00c9a38465f146bcb97f2050
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2020-12-07 12:32:31 -0500

    update link, remove ogen-cli

--- ogen_setup_testnet.sh
+++ ogen_setup_testnet.sh
@@ -1,5 +1,5 @@
 TMP_FOLDER=$(mktemp -d)
-COIN_REPO='https://public.oly.tech/ogen-release/ogen-0.1.0-alpha.2-linux-amd64.tar.gz'
+COIN_REPO='https://github.com/olympus-protocol/ogen/releases/download/v0.1.2-alpha.8/ogen-0.1.2-alpha.8-linux-amd64.tar.gz'
 COIN_NAME='Olympus'
 COIN_DAEMON='ogen'
 RED='\033[0;31m'
@@ -33,7 +33,6 @@ function install_node() {
   tar xvf $COIN_ZIP --strip 2 >/dev/null 2>&1
   compile_error
   cp ogen /usr/local/bin
-  cp ogen-cli /usr/local/bin
   compile_error
   cd - >/dev/null 2>&1
   rm -rf $TMP_FOLDER >/dev/null 2>&1
